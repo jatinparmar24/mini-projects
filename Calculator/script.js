dc27f8bea757b36107f68310c9bd941a93ddf58c
jatinparmar24 <jatinparmar2424@gmail.com>
2025-05-05 12:31:37 +0530

    adding details in my calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,46 +1,36 @@
+let store = "";
 
 
-
-
-
-let store=""
-
-let cal=(value)=>{
-
- let show=document.querySelector("#display")
- store=store+value;
- show.innerHTML=store;
-
+let cal = (value) => {
+  let show = document.querySelector("#display");
+  store = store + value;
+  show.value = store;  
 }
 
-let result=()=>{
-
-let show=document.querySelector("#display")
-show.innerHTML=eval(store)     //  eval gives number =  "1+2"=3(number)
-store=eval(store)    // result is number like 3
-store.toString()  // it change number into string
-
+let result = () => {
+  let show = document.querySelector("#display");
+  try {
+    show.value = eval(store);  
+    store = show.value;  
+  } catch (e) {
+    show.value = "Error";  
+  }
 }
 
-let del=()=>{
-
-    let show=document.querySelector("#display")
-    store=store.slice(0,-1)
-    show.innerHTML=store;
-
+let del = () => {
+  let show = document.querySelector("#display");
+  store = store.slice(0, -1);  
+  show.value = store;
 }
 
-let allc=()=>{
-  
-    let show=document.querySelector("#display")
-    store="";
-    show.innerHTML=store;
-    
+let allc = () => {
+  let show = document.querySelector("#display");
+  store = ""; 
+  show.value = store; 
 }
 
-let squ=()=>{
-    let show=document.querySelector("#display")
-    store=store*store;
-    show.innerHTML=store;
-    
+let squ = () => {
+  let show = document.querySelector("#display");
+  store = Math.pow(store, 2); 
+  show.value = store; 
 }
